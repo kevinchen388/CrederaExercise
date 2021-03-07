5ecace8f56a7a31fafef2f628dca57d98c233ed8
Kevin Chen <kevin.c388@gmail.com>
2021-03-07 18:23:55 -0500

    HTML/CSS

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log(json_data)
+
+
+
+// fetch('../Credera/content.json')
+//  .then(function (response) {
+//    return response.json();
+//  })
+//  .then(function (data) {
+//    console.log(data)
+//    appendData(data);
+//  })
+//  .catch(function (err) {
+//    console.log(err);
+//  });
+
+
+// function appendData(data) {
+//  var mainContainer = document.getElementById("myData");
+//  for (var i = 0; i < data.length; i++) {
+//    var div = document.createElement("div");
+//    div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+//    mainContainer.appendChild(div);
+//  }
+// }
+
+
---
+++ content.js
@@ -0,0 +1,47 @@
+const json_data = {
+    "cards": [
+        {
+            "title": "Google is Building Quantum Computing Processors",
+            "description": "Google has teamed up with a group of university scientists in California in an effort to build quantum information processors…",
+            "date_published" : "2015-08-10T12:00:00Z",
+            "image_url": "images/quantum_computer-shutterstock2.jpg",
+            "link": "https://www.credera.com/"
+        },
+        {
+            "title": "Google is Building Quantum Computing Processors",
+            "description": "Google has teamed up with a group of university scientists in California in an effort to build quantum information processors…",
+            "date_published" : "2015-08-10T12:00:00Z",
+            "image_url": "images/quantum_computer-shutterstock2.jpg",
+            "link": "https://www.credera.com/"
+        },
+        {
+            "title": "Google is Building Quantum Computing Processors",
+            "description": "Google has teamed up with a group of university scientists in California in an effort to build quantum information processors…",
+            "date_published" : "2015-08-10T12:00:00Z",
+            "image_url": "images/quantum_computer-shutterstock2.jpg",
+            "link": "https://www.credera.com/"
+        },
+        {
+            "title": "Google is Building Quantum Computing Processors",
+            "description": "Google has teamed up with a group of university scientists in California in an effort to build quantum information processors…",
+            "date_published" : "2015-08-10T12:00:00Z",
+            "image_url": "images/quantum_computer-shutterstock2.jpg",
+            "link": "https://www.credera.com/"
+        },
+        {
+            "title": "Google is Building Quantum Computing Processors",
+            "description": "Google has teamed up with a group of university scientists in California in an effort to build quantum information processors…",
+            "date_published" : "2015-08-10T12:00:00Z",
+            "image_url": "images/quantum_computer-shutterstock2.jpg",
+            "link": "https://www.credera.com/"
+        },
+        {
+            "title": "Google is Building Quantum Computing Processors",
+            "description": "Google has teamed up with a group of university scientists in California in an effort to build quantum information processors…",
+            "date_published" : "2015-08-10T12:00:00Z",
+            "image_url": "images/quantum_computer-shutterstock2.jpg",
+            "link": "https://www.credera.com/"
+        }      
+        
+    ]
+};
